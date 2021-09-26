60abe09789b38d231a9c02016bcdd93dc5a60b56
Xinfei Li <77464299+xlif1121@users.noreply.github.com>
2021-09-25 21:25:48 -1000

    9/25

---
+++ Lab7/Ex1.js
@@ -0,0 +1,11 @@
+var age =  20;
+
+var fav_num = 21;
+
+var day_of_birth = 21;
+
+var month_of_birth = 11;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+
+console.log((age + fav_num) / day_of_birth * month_of_birth);
