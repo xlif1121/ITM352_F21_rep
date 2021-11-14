83565ee70ebe50b009aeb8e7619d637c8c1d9c77
Xinfei Li <77464299+xlif1121@users.noreply.github.com>
2021-11-13 22:39:47 -1000

    11/13

--- Li_Xinfei_Assignment1/server.js
+++ Li_Xinfei_Assignment1/server.js
@@ -1,7 +1,10 @@
 const { response } = require('express');
 var express = require('express');
 var app = express();
-
+var myParser = require("body-parser");
+// get products.js
+var products =require('./public/products');
+var querystring = require("querystring");
 // Routing 
 
 // monitor all requests
@@ -9,9 +12,6 @@ app.all('*', function (request, response, next) {
    console.log(request.method + ' to ' + request.path);
    next();
 });
-
-// process purchase request (validate quantities, check quantity available)
-app.get('./public/invoice.html', function (req, res, next) {
 
     function isNonNegInt(q, return_errors = false) {
         errors = []; // assume no errors at first
@@ -23,12 +23,34 @@ app.get('./public/invoice.html', function (req, res, next) {
         return return_errors ? errors : (errors.length == 0);
     }
 
-    data = require('./public/product.js');
-    products = data.products;
-});
+    app.use(myParser.urlencoded({ extended: true })); 
+// process purchase request (validate quantities, check quantity available)
+    app.post("/process_form", function (req, res, next) {
+    // check the quantity, if it is not valid, send it to the products display page in order to repurchase
+    let POST = req.body;
+    var validQu=true;
+    var purchased =false;
+
+    for (i = 0; i < products.length; i++) {
+        qua = POST['quantity' + i]; 
+        if (isNonNegInt(qua) == false) { 
+            validQu = false; 
+        }
+        if (qua> 0) { 
+            purchased = true; 
+        }
+    }
+    QString = querystring.stringify(POST); //in order to stringing the query
+    if (validQu == true && purchased == true) { 
+        res.redirect("./invoice.html?" + QString); // if it is valid, send to invoice
+    }else { 
+        res.redirect("./products_display.html?" + QString); // if it is incorrect, send to products display html
+    }
+ });
+
 
 // route all other GET requests to files in public 
 app.use(express.static('./public'));
 
 // start server
-var listener = app.listen(8080, () => { console.log('server started listening on port ' + listener.address().port) });
+app.listen(8080, () => console.log(`listening on port 8080`));
